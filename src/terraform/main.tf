3aaba615ce8ae37cd8c3113d7057fa103cb7a9b8
kiahsa2002 <kiahsa2002@outlook.com>
2019-10-28 20:35:34 +0100

    Added the worker configuration along recent changes in the master configuration

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -2,7 +2,7 @@ resource "openstack_compute_keypair_v2" "my-cloud-key" {
   name       = "accg14key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOk6VdPxND/vovKrZbvLl5ot8sLEB4SZxd2b9Xk6RIJMnLr9hd/PcFzNPrXh/t/ZG/uzUwlUl7s/+xFLxW8nYRutn7BOif9MZUgSU+n13tVgqHdnwIhbTERIoLgye39tlKk3DkRjWxJ0HZ9YzeC2cMSV2UniJ1qmzEpiZxlWSDJleEUVAjl12A9wqOw21pidXpwzSJ0aAsPYVDv2Pyufv7Fqf/xgJNVJzWiJGKiqdP0HE/khHSdz+ad7Ea/4u5QTkwj5n+PN43EmaCbroEBBf237tZMiEXrS3jlIjUVKXMM+Pga/XLwlTrYxZDLbDzbrxbDMYYj+P+py88wbYEiOBz"
 }
-resource "openstack_compute_instance_v2" "test" {
+resource "openstack_compute_instance_v2" "master" {
   name            = "master-vm"
   image_name      = "Ubuntu 18.04 LTS (Bionic Beaver) - latest"
   flavor_name     = "ssc.small"
@@ -14,23 +14,14 @@ resource "openstack_compute_instance_v2" "test" {
     name = "SNIC 2019/10-13 Internal IPv4 Network"
   }
 }
-resource "openstack_compute_instance_v2" "nfs" {
-  name            = "nfs-vm"
-  image_name      = "Ubuntu 18.04 LTS (Bionic Beaver) - latest"
-  flavor_name     = "ssc.small"
-  key_pair        = "${openstack_compute_keypair_v2.my-cloud-key.name}"
-  security_groups = ["default"]
 
-  network {
-    name = "SNIC 2019/10-13 Internal IPv4 Network"
-  }
-}
 resource "openstack_compute_instance_v2" "wrkr" {
   name            = "wrkr-${count.index}"
   image_name      = "Ubuntu 18.04 LTS (Bionic Beaver) - latest"
   flavor_name     = "ssc.small"
   key_pair        = "${openstack_compute_keypair_v2.my-cloud-key.name}"
   security_groups = ["default"]
+  user_data       = "${file("worker.conf")}"
   count		  = 3
 
   network {
